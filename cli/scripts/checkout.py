8aaaced93c0f4d073d0c5dbc21f95606ea774ded
NWBY <sam.newby19@gmail.com>
2022-03-19 14:39:24 +0000

    Add checkout command with optional flag to create branches

--- cli/scripts/checkout.py
+++ cli/scripts/checkout.py
@@ -3,10 +3,13 @@ import subprocess
 
 @click.command()
 @click.argument('branch')
-def ch(branch):
-    cmd = subprocess.Popen(['git', 'checkout', branch],
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE)
+@click.option('-c', '--create', default=False, type=bool)
+def ch(branch, create):
+    if create:
+        cmd = subprocess.Popen(['git', 'checkout', '-b', branch], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    else:
+        cmd = subprocess.Popen(['git', 'checkout', branch], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
     stdout, stderr = cmd.communicate()
     
     click.echo(f'Checked out {branch} bad boy')
